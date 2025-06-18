985942d526271515c7c2c2aa1ccd650055fdba9b
GGB0T11 <gusttrabs@gmail.com>
2025-06-17 21:05:39 -0300

    agora vai ser tudo feito em django python

---
+++ projeto/estoque/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Usuario(AbstractUser):
+    CARGOS = [("func", "Funcionário"),("adm", "Administrador")]
+    cargo = models.CharField(max_length=4, choices=CARGOS, default="func")
+
+    def __str__(self):
+        return f"{self.email}: {self.cargo}"
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Insumo(models.Model):
+    nome = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    qteAtual = models.IntegerField(default=0)
+    unidadeMedida = models.CharField(max_length=10)
+    ativo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    insumos = models.ManyToManyField(Insumo)
+
+    def __str__(self):
+        return self.nome
+
+class Movimentacao(models.Model):
+    TIPOS = [("entrada", "Entrada"), ("saida", "Saida")]
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=7, choices=TIPOS)
+    quantidade = models.IntegerField(default=1)
+    data = models.DateTimeField(auto_now_add=True)
+    comentario = models.TextField(null=True)
+
+    def __str__(self):
+        return f"{self.produto}: {self.quantidade} {self.tipo}"
