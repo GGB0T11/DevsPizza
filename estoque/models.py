9c310d336d7ee4dcd4b1c0b45196f56aee7189c2
GGB0T11 <gusttrabs@gmail.com>
2025-06-20 11:53:49 -0300

    ao remover uma categoria o insumo fica sem categoria

--- estoque/models.py
+++ estoque/models.py
@@ -9,7 +9,7 @@ class Categoria(models.Model):
 
 class Insumo(models.Model):
     nome = models.CharField(max_length=100)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, null=True, on_delete=models.SET_NULL)
     qte_atual = models.PositiveIntegerField(default=0)
     unidade_medida = models.CharField(max_length=10)
     ativo = models.BooleanField(default=True)
