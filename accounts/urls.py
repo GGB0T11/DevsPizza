3641d96426216681ecb91c440b716ab1567eccb5
GGB0T11 <gusttrabs@gmail.com>
2025-06-24 14:55:34 -0300

    CRUD simples do accounts, fazer os TODO

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import login, register
+from .views import delete_account, detail_account, list_account, login, register, update_account
 
 urlpatterns = [
     path("login/", login, name="login"),
     path("register/", register, name="register"),
+    path("", list_account, name="list"),
+    path("<int:id>", detail_account, name="detail"),
+    path("<int:id>/update", update_account, name="update"),
+    path("<int:id>/delete", delete_account, name="delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,24 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
-from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_http_methods
 
 from .models import CustomUser
+
+# TODO: Tratar o erro 404
+# TODO: Usar forms
 
 
 @require_http_methods(["GET", "POST"])
 def login(request):
     if request.method == "GET":
-        return render(request, "login.html")
+        if request.user.is_authenticated:
+            return redirect("home")
+
+        else:
+            return render(request, "login.html")
 
     else:
         email = request.POST.get("email")
@@ -27,6 +35,7 @@ def login(request):
             return redirect("login")
 
 
+# TODO: Fazer verificações mais rígidas
 @require_http_methods(["GET", "POST"])
 def register(request):
     if request.method == "GET":
@@ -65,3 +74,59 @@ def register(request):
 
         messages.success(request, "Usuário criado com sucesso!")
         return redirect("home")
+
+
+# TODO: Fazer paginação
+@login_required
+@require_http_methods(["GET"])
+def list_account(request):
+    accounts = CustomUser.objects.all()
+
+    if accounts:
+        return render(request, "list.html", {"accounts": accounts})
+
+    else:
+        messages.error(request, "Nenhuma conta criada")
+        redirect(request, "list.html")
+
+
+@login_required
+@require_http_methods(["GET"])
+def detail_account(request, id):
+    account = get_object_or_404(CustomUser, id=id)
+
+    return render(request, "detail.html", {"account": account})
+
+
+# TODO: Fazer o html do update
+@login_required
+@require_http_methods(["GET", "POST"])
+def update_account(request, id):
+    account = get_object_or_404(CustomUser, id=id)
+
+    if request.method == "GET":
+        return render(request, "update.html", {"account": account})
+
+    else:
+        account.first_name = request.POST.get("first_name")
+        account.last_name = request.POST.get("last_name")
+        account.email = request.POST.get("email")
+        account.role = request.POST.get("role")
+
+        account.save()
+
+        messages.success(request, "Conta alterada com sucesso!")
+        return redirect("list")
+
+
+# TODO: Solicitar confirmação
+# Acho que essa lógica está errada mas por enquanto vou manter
+@login_required
+@require_http_methods(["GET"])
+def delete_account(request, id):
+    account = get_object_or_404(CustomUser, id=id)
+
+    account.delete()
+
+    messages.success(request, "Conta deletada com sucesso!")
+    return redirect("list")
