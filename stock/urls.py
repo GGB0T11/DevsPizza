25b603a0fc095061e10f981ccabf0223df43fe91
GGB0T11 <gusttrabs@gmail.com>
2025-06-29 10:00:46 -0300

    CRUD dos Ingredients e Products

--- stock/urls.py
+++ stock/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from .views import category_create, category_delete, category_detail, category_list, category_update
+from . import views
 
 urlpatterns = [
-    path("category/new", category_create, name="category_create"),
-    path("category/", category_list, name="category_list"),
-    path("category/<int:id>", category_detail, name="category_detail"),
-    path("category/<int:id>/update", category_update, name="category_update"),
-    path("category/<int:id>/delete", category_delete, name="category_delete"),
+    path("category/new", views.category_create, name="category_create"),
+    path("category/", views.category_list, name="category_list"),
+    path("category/<int:id>", views.category_detail, name="category_detail"),
+    path("category/<int:id>/update", views.category_update, name="category_update"),
+    path("category/<int:id>/delete", views.category_delete, name="category_delete"),
+    path("ingredient/new", views.ingredient_create, name="ingredient_create"),
+    path("ingredient/", views.ingredient_list, name="ingredient_list"),
+    path("ingredient/<int:id>", views.ingredient_detail, name="ingredient_detail"),
+    path("ingredient/<int:id>/update", views.ingredient_update, name="ingredient_update"),
+    path("ingredient/<int:id>/delete", views.ingredient_delete, name="ingredient_delete"),
+    path("product/new", views.product_create, name="product_create"),
+    path("product/", views.product_list, name="product_list"),
+    path("product/<int:id>", views.product_detail, name="product_detail"),
+    path("product/<int:id>/update", views.product_update, name="product_update"),
+    path("product/<int:id>/delete", views.product_delete, name="product_delete"),
 ]
--- stock/views.py
+++ stock/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.http import require_http_methods
 
 from .models import Category, Ingredient, Product
 
+# TODO: Filtragem e busca nas lists
+# TODO: 404 mais amigavel e detalhado
+
 
 # TODO: Fazer formatação e validação no nome
 @login_required
@@ -82,9 +85,151 @@ def ingredient_create(request):
     if request.method == "GET":
         categories = Category.objects.all()
 
-        return render(request, "ingredient_create", {"categories": categories})
+        return render(request, "ingredient_create.html", {"categories": categories})
 
     else:
-        return       
+        name = request.POST.get("name")
+        qte = request.POST.get("qte")
+        category_id = request.POST.get("category")
+        measure_unit = request.POST.get("measure_unit")
 
-    return
+        category = get_object_or_404(Category, id=category_id)
+
+        if Ingredient.objects.filter(name=name):
+            messages.error(request, "O ingrediente que deseja cadastrar já existe!")
+            return redirect("ingredient_list")
+
+        ingredient = Ingredient.objects.create(name=name, current_qte=qte, category=category, measure_unit=measure_unit)
+
+        ingredient.save()
+
+        messages.success(request, "Ingrediente cadastrado com sucesso!")
+        return redirect("ingredient_list")
+
+
+@login_required
+@require_http_methods(["GET"])
+def ingredient_list(request):
+    ingredients = Ingredient.objects.all()
+
+    return render(request, "ingredient_list.html", {"ingredients": ingredients})
+
+
+@login_required
+@require_http_methods(["GET"])
+def ingredient_detail(request, id):
+    ingredient = get_object_or_404(Ingredient, id=id)
+
+    return render(request, "detail.html", {"ingredient": ingredient})
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def ingredient_update(request, id):
+    ingredient = get_object_or_404(Ingredient, id=id)
+    categories = Category.objects.all()
+
+    if request.method == "GET":
+        return render(request, "ingredient_update.html", {"ingredient": ingredient, "categories": categories})
+
+    else:
+        ingredient.name = request.POST.get("name")
+        category_id = request.POST.get("category")
+        ingredient.category = get_object_or_404(Category, id=category_id)
+        ingredient.measure_unit = request.POST.get("measure_unit")
+
+        if request.POST.get("active") == "True":
+            ingredient.active = True
+
+        else:
+            ingredient.active = False
+
+        ingredient.save()
+
+        messages.success(request, "Ingrediente alterado com sucesso!")
+        return redirect("ingredient_list")
+
+
+@login_required
+@require_http_methods(["GET"])
+def ingredient_delete(request, id):
+    ingredient = get_object_or_404(Ingredient, id=id)
+
+    ingredient.delete()
+
+    messages.success(request, "Ingrediente deletado com sucesso!")
+    return redirect("ingredient_list")
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def product_create(request):
+    if request.method == "GET":
+        ingredients = Ingredient.objects.all()
+        return render(request, "product_create.html", {"ingredients": ingredients})
+
+    else:
+        name = request.POST.get("name")
+        ingredients_ids = request.POST.getlist("ingredients")
+        ingredients = Ingredient.objects.filter(id__in=ingredients_ids)
+
+        product = Product.objects.create(name=name)
+        product.ingredients.set(ingredients)
+        product.save()
+
+        messages.success(request, "Produto criado com sucesso!")
+        return redirect("product_list")
+
+
+@login_required
+@require_http_methods(["GET"])
+def product_list(request):
+    products = Product.objects.all()
+
+    return render(request, "product_list.html", {"products": products})
+
+
+@login_required
+@require_http_methods(["GET"])
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    return render(request, "product_detail", {"product": product})
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def product_update(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == "GET":
+        ingredients = Ingredient.objects.all()
+        ingredients_ids = product.ingredients.values_list("id", flat=True)
+
+        return render(
+            request,
+            "product_update.html",
+            {"product": product, "ingredients": ingredients, "ingredients_ids": ingredients_ids},
+        )
+
+    else:
+        product.name = request.POST.get("name")
+        ingredients_ids = request.POST.getlist("ingredients")
+        ingredients = Ingredient.objects.filter(id__in=ingredients_ids)
+        product.ingredients.set(ingredients)
+
+        product.save()
+
+        messages.success(request, "Produto alterado com sucesso!")
+        return redirect("product_list")
+
+
+@login_required
+@require_http_methods(["GET"])
+def product_delete(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    product.delete()
+
+    messages.success(request, "Produto deletado com sucesso!")
+    return redirect("product_list")
